7b314182351bb927388f9deeac45647a290247e2
Kaz Baig <khizermbaig@gmail.com>
2021-08-21 14:20:36 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ClockClock } from "./ClockClock";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <ClockClock />
+    </ChakraProvider>
+  );
+}
+
+export default App;
